bcca9a446e40f6928810002a0a3b2906b8a91a1e
pranjal <pranjalmaurya01@gmail.com>
2023-10-09 15:44:51 +0530

    updated url

--- src/axios/baseUrl.ts
+++ src/axios/baseUrl.ts
@@ -1,4 +1,4 @@
 const getBaseUrl = (): string =>
-	'https://bab0-2401-4900-1f34-46a3-1c5a-80e9-5da9-61f0.ngrok-free.app/';
+	'https://ef90-2a02-4780-28-6560-00-1.ngrok-free.app/';
 
 export default getBaseUrl;
